364281e3fa6b717a3786a408e0837f6bd44d4446
Sarah Prando <sarahprando40@gmail.com>
2024-02-19 19:00:32 -0300

    update search

--- assents/js/script.js
+++ assents/js/script.js
@@ -2,10 +2,12 @@
 
 const lupaSearch = document.querySelector('.search img');
 const inputSearch = document.querySelector('.search input');
+const divSearch = document.querySelector('.search')
 let check = 0;
 
 function searchOpen(event) {
     inputSearch.classList.toggle('ativo');
+divSearch.classList.toggle('borda');
 }
 
 function searchCheck(event) {
@@ -15,6 +17,7 @@ function searchCheck(event) {
 function searchClose(event) {
     if (check == 0) {
         inputSearch.classList.remove('ativo');
+divSearch.classList.remove('borda');
     }
 }
 
